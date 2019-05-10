3c2a94fe7eaa6264786fdb9ac666b88c1d8e31ef
Butty <mark@markbutterworth.net>
2019-05-10 10:58:55 +0100

    Added some descriptions to output

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import mylib
 import mysound
 import mysound.manage
 from mysound.formats import *
-import mysound.effects.echo as eo
+from mysound.effects import *
+import mysound.effects.echo as ee
 import mysound.effects.echo
 
 
@@ -11,31 +12,33 @@ print(f'Now initialising {__name__}')
 
 
 def main():
-    print(dir())
-    print(dir(mylib))
+    mainflag = True
+    print(f'dir in main function: {dir()}')
+    print(f'dir of mylib: {dir(mylib)}')
     mylib.sayhello(__name__)
-    print(dir(mysound))
-    print(dir(mysound.manage))
+    print(f'dir of mysound: {dir(mysound)}')
+    print(f'dir of mysound.manage: {dir(mysound.manage)}')
     mysound.manage.sayhello(__name__)
-    print(dir(wav))
+    print(f'dir of wav: {dir(wav)}')
     wav.sayhello(__name__)
-    print(dir(mp3))
+    print(f'dir of mp3: {dir(mp3)}')
     mp3.sayhello(__name__)
-    print(dir(eo))
-    print(dir(mysound.effects.echo))
-    print(f'eo is echo: {eo is mysound.effects.echo}')
+    print(f'dir of ee: {dir(ee)}')
+    print(f'dir of mysound.effects.echo: {dir(mysound.effects.echo)}')
+    print(f'ee is echo: {ee is mysound.effects.echo}')
 
-    eo.sayhello(__name__)
-    print(f'Volume is {eo.volume}')
-    eo.volume = 11
+    ee.sayhello(__name__)
+    print(f'Volume is {ee.volume}')
+    ee.volume = 11
     print(f'Volume is {mysound.effects.echo.volume}')
 
     def goodbye(s):
         print(f'Goodbye {s} from {__name__}')
     
-    eo.sayhello = goodbye
-    eo.sayhello(__name__)
+    ee.sayhello = goodbye
+    ee.sayhello(__name__)
 
 
 if __name__ == '__main__':
+    print(f'dir in {__name__}: {dir()}')
     sys.exit(main())
--- mysound/__init__.py
+++ mysound/__init__.py
@@ -1,3 +1,8 @@
 
 
 print(f'Now initialising {__name__}')
+
+DEFVOLUME = 11
+
+def setoptions(opt, value):
+    print('f Setting {opt} to {value}')
