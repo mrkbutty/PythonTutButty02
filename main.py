31e20e0c06f20a7777ac8edd431129db81c6ae01
Butty <mark@markbutterworth.net>
2020-07-25 13:45:05 +0100

    Updated for remote class

--- main.py
+++ main.py
@@ -12,28 +12,34 @@ print(f'Now initialising {__name__}')
 
 
 def main():
+    print(f'\nPython search path for library modules: {sys.path}\n')
+    
     mainflag = True
     print(f'dir in main function: {dir()}')
     print(f'dir of mylib: {dir(mylib)}')
     mylib.sayhello(__name__)
-    print(f'dir of mysound: {dir(mysound)}')
+    
+    print(f'\ndir of mysound: {dir(mysound)}')
     print(f'dir of mysound.manage: {dir(mysound.manage)}')
     mysound.manage.sayhello(__name__)
-    print(f'dir of wav: {dir(wav)}')
+    
+    print(f'\ndir of wav: {dir(wav)}')
     wav.sayhello(__name__)
-    print(f'dir of mp3: {dir(mp3)}')
+    
+    print(f'\ndir of mp3: {dir(mp3)}')
     mp3.sayhello(__name__)
-    print(f'dir of ee: {dir(ee)}')
+    
+    print(f'\ndir of ee: {dir(ee)}')
     print(f'dir of mysound.effects.echo: {dir(mysound.effects.echo)}')
     print(f'ee is echo: {ee is mysound.effects.echo}')
 
     ee.sayhello(__name__)
-    print(f'Volume is {ee.volume}')
+    print(f'\nVolume is {ee.volume}')
     ee.volume = 11
     print(f'Volume is {mysound.effects.echo.volume}')
 
     def goodbye(s):
-        print(f'Goodbye {s} from {__name__}')
+        print(f'\nGoodbye {s} from {__name__}')
     
     ee.sayhello = goodbye
     ee.sayhello(__name__)
